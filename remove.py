8e55e39489dd34275664da3b228769b6212fb881
Ning <ning328@gmail.com>
2020-06-04 12:05:21 +0100

    added script to remove existing dotfiles

--- remove.py
+++ remove.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import yaml
 import os
 
-CONFIG="install.conf.yaml"
+CONFIG="install.test.yaml"
 
 stream = open(CONFIG, "r")
 conf = yaml.load(stream)
@@ -13,11 +13,13 @@ for section in conf:
     if 'link' in section:
         for target in section['link']:
             realpath = os.path.expanduser(target)
+            print(realpath)
             if os.path.islink(realpath):
                 print("Removing ", realpath)
                 os.unlink(realpath)
-            # else:
-                # if os.path.exists(realpath):
-                    # os.remove(realpath)
-                # else:
-                    # print("Can not delete the file as it doesn't exists")
+            if os.path.exists(realpath) and os.path.isfile(realpath):
+                os.remove(realpath)
+                print("delete the file ")
+            if os.path.exists(realpath) and os.path.isdir(realpath):
+                os.rmdir(realpath)
+                print("delete the dir")
